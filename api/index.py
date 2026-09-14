8d9fb66d4c502109577d1d83c4f5bd8f5cff6a9e
Zaib_Shahid <zaibshahid123@gmail.com>
2026-09-14 12:33:26 +0500

    remove runtime collectstatic call, static files now pre-built

--- api/index.py
+++ api/index.py
@@ -9,11 +9,5 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mongo_crud_project.settings')
 import django
 django.setup()
 
-from django.core.management import call_command
-try:
-    call_command('collectstatic', interactive=False, verbosity=0)
-except Exception as e:
-    print(f"collectstatic warning: {e}")
-
 from django.core.wsgi import get_wsgi_application
 app = get_wsgi_application()
